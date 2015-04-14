69c582617bfec2e90bb8d8afc15997c3249f0d73
dylanmensaert <dylan.mensaert@gmail.com>
2015-04-15 01:06:26 +0200

    Further modified layout.

--- assets/javascripts/index/controller.js
+++ assets/javascripts/index/controller.js
@@ -7,32 +7,47 @@ define(function(require) {
     return Ember.Controller.extend({
         query: '',
         snippets: null,
+        musicOnly: true,
+        search: function() {
+            var url = metaData.searchHost + '/youtube/v3/search?part=snippet&order=viewCount&type=video';
+
+            // TODO: implement in settings page?
+            if (this.get('musicOnly')) {
+                url += '&videoCategoryId=10';
+            }
+
+            url += '&key=' + metaData.key;
+            url += '&q=' + this.get('query');
+
+            // TODO: url += '&relatedToVideoId=' + this.get('videoId');
+
+            Ember.$.getJSON(url).then(function(response) {
+                var snippets = response.items.map(function(item) {
+                    return Ember.Object.create({
+                        videoId: item.id.videoId,
+                        title: item.snippet.title,
+                        thumbnail: item.snippet.thumbnails.high.url
+                    });
+                });
+
+                this.set('snippets', snippets);
+            }.bind(this));
+        },
         actions: {
             search: function() {
-                var url = metaData.searchHost + '/youtube/v3/search?part=snippet&order=viewCount&type=video&videoCategoryId=10';
-
-                url += '&key=' + metaData.key;
-                url += '&q=' + this.get('query');
-
-                // TODO: url += '&relatedToVideoId=' + this.get('videoId');
-
-                Ember.$.getJSON(url).then(function(response) {
-                    var snippets = response.items.map(function(item) {
-                        return Ember.Object.create({
-                            videoId: item.id.videoId,
-                            title: item.snippet.title,
-                            thumbnail: item.snippet.thumbnails.high.url
-                        });
-                    });
-
-                    this.set('snippets', snippets);
-                }.bind(this));
+                this.search();
             },
+            // TODO: set class 'active' on currently playing
             load: function(snippet) {
                 this.get('audio').load(snippet);
             },
             clear: function() {
                 this.set('query', '');
+            },
+            toggleMusicOnly: function() {
+                this.toggleProperty('musicOnly');
+
+                this.search();
             }
         }
     });
