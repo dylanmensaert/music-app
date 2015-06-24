1152912e90ecf2b48b8f60f3dfdf13e815166d7b
dylanmensaert <dylan.mensaert@cloudinnovation.be>
2015-06-24 19:50:19 +0200

    Implemented message destroy on click.

--- assets/javascripts/message-bar/component.js
+++ assets/javascripts/message-bar/component.js
@@ -2,22 +2,26 @@ define(function(require) {
     'use strict';
 
     var Ember = require('ember'),
-        fadeOut;
-
-    fadeOut = function() {
-        this.$().fadeOut(500, function() {
-            this.set('content', null);
-        }.bind(this));
-    };
+        debouncer;
 
     return Ember.Component.extend({
         layoutName: 'messageBar',
         classNames: ['message-bar'],
         content: null,
+        fadeOut: function() {
+            this.$().fadeOut(500, function() {
+                this.set('content', null);
+            }.bind(this));
+        },
         update: function() {
             if (!Ember.isEmpty(this.get('content'))) {
-                Ember.run.debounce(this, fadeOut, 2000);
+                debouncer = Ember.run.debounce(this, this.fadeOut, 2000);
             }
         }.observes('content').on('didInsertElement'),
+        click: function() {
+            Ember.run.cancel(debouncer);
+
+            this.fadeOut();
+        }
     });
 });
