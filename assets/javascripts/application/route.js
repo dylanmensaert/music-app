2c17bbdc84474f7be9354a15d19daf9c267b8c23
dylanmensaert <dylan.mensaert@gmail.com>
2015-07-01 01:21:49 +0200

    Implemented next and previous.

--- assets/javascripts/application/route.js
+++ assets/javascripts/application/route.js
@@ -3,7 +3,12 @@ define(function(require) {
     'use strict';
 
     var Ember = require('ember'),
-        Slider = require('slider/object');
+        Slider = require('slider/object'),
+        generateRandom;
+
+    generateRandom = function(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
 
     return Ember.Route.extend(require('helpers/update-title'), {
         title: 'music',
@@ -36,42 +41,75 @@ define(function(require) {
             this._super(controller, model);
         },
         previous: function() {
-            var audio = this.get('audio'),
-                queue = this.get('fileSystem.queue'),
-                currentIndex = queue.indexOf(audio.get('snippet.id')),
+            var history = this.get('fileSystem.history'),
+                queue,
+                currentIndex,
                 previousIndex,
+                snippetId,
                 previousSnippet;
 
-            if (currentIndex > 0) {
-                previousIndex = currentIndex - 1;
+            if (history.get('length') > 1) {
+                snippetId = history.objectAt(history.get('length') - 2);
             } else {
-                previousIndex = queue.get('length');
+                queue = this.get('fileSystem.queue');
+                currentIndex = queue.indexOf(this.get('audio.snippet.id'));
+
+                if (currentIndex > 0) {
+                    previousIndex = currentIndex - 1;
+                } else {
+                    previousIndex = queue.get('length');
+                }
+
+                snippetId = queue.objectAt(previousIndex);
             }
 
-            previousSnippet = this.get('fileSystem.snippets').findBy('id', queue.objectAt(previousIndex));
+            previousSnippet = this.get('fileSystem.snippets').findBy('id', snippetId);
 
             this.play(previousSnippet);
         },
         next: function() {
-            var audio = this.get('audio'),
-                queue = this.get('fileSystem.queue'),
-                currentIndex = queue.indexOf(audio.get('snippet.id')),
+            var queue = this.get('fileSystem.queue'),
+                currentIndex,
                 nextIndex,
+                playedSnippetIds,
+                snippetId,
                 nextSnippet;
 
-            if (currentIndex < queue.get('length')) {
-                nextIndex = currentIndex + 1;
-            } else {
-                nextIndex = 0;
+            if (this.get('fileSystem.isRepeating')) {
+                currentIndex = queue.indexOf(this.get('audio.snippet.id'));
+
+                if (currentIndex < queue.get('length')) {
+                    nextIndex = currentIndex + 1;
+                } else {
+                    nextIndex = 0;
+                }
+
+                snippetId = queue.objectAt(nextIndex);
+            } else if (this.get('fileSystem.isShuffling')) {
+                playedSnippetIds = this.get('cache.playedSnippetIds');
+
+                unplayedSnippetIds = queue.filter(function(snippetId) {
+                    return !playedSnippetIds.contains(snippetId);
+                });
+
+                if (!unplayedSnippetIds.get('length')) {
+                    this.set('cache.playedSnippetIds', []);
+
+                    unplayedSnippetIds.pushObjects(queue);
+
+                    unplayedSnippetIds.removeObject(this.get('audio.snippet.id'));
+                }
+
+                snippetId = unplayedSnippetIds.objectAt(generateRandom(0, unplayedSnippetIds.get('length') - 1));
             }
 
-            nextSnippet = this.get('fileSystem.snippets').findBy('id', queue.objectAt(nextIndex));
+            nextSnippet = this.get('fileSystem.snippets').findBy('id', snippetId);
 
             this.play(nextSnippet);
         },
         play: function(snippet) {
             var history,
-            playedSnippetIds,
+                playedSnippetIds,
                 id;
 
             if (!Ember.isEmpty(snippet)) {
